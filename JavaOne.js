3667aeec90461a09f04852635af1ac049aad7175
KaliVenable <31870113+KaliVenable@users.noreply.github.com>
2017-11-13 18:56:23 -0600

    Added Steps 4-7

--- JavaOne.js
+++ JavaOne.js
@@ -1,15 +1,25 @@
 <html>
 <script>
-// question one
-var x = 5
+var x = 5;
 /* This defines x as five */
-// question one
-var y = 3
-/* This defines 7 as three */
-// question one
-var z = x * y
+var y = 3;
+/* This defines y as three */
+var z = x * y;
 /* This defines z as x times y */
-console, log(z)
+console, log(z);
 /* this logs value of z to console */
+var a = 4;
+/* This defines a as four*/
+var b = 8;
+/* This defines b as eight */
+var c = a * b;
+/* This defines c as a times b */
+console, log(c);
+x = 10;
+/* This defines x as ten */
+y = 6;
+/* This defines y as six*/
+var u = x * y;
+/* This defines u as x times y */
 </Script>
 </html>
