8088eccb683226161abad8f018f1c3e9954632e3
KaliVenable <31870113+KaliVenable@users.noreply.github.com>
2017-11-13 18:49:43 -0600

    Math exercises one through seven

---
+++ JavaOne.js
@@ -0,0 +1,15 @@
+<html>
+<script>
+// question one
+var x = 5
+/* This defines x as five */
+// question one
+var y = 3
+/* This defines 7 as three */
+// question one
+var z = x * y
+/* This defines z as x times y */
+console, log(z)
+/* this logs value of z to console */
+</Script>
+</html>
